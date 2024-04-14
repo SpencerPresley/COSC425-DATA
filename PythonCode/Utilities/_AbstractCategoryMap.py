ee31a0290ff933cc3747290d6b77cac9b87d3b81
cbarbes <cbarbes02@gmail.com>
2024-04-14 19:00:35 -0400

    Tested the prompt to get better output, added individual analysis function, changed the way the data is given so now title is the key

--- PythonCode/Utilities/_AbstractCategoryMap.py
+++ PythonCode/Utilities/_AbstractCategoryMap.py
@@ -1,7 +1,7 @@
 import os
 
 import sys
-sys.path.append('/home/cole/Desktop/COSC425-DATA/')
+sys.path.append('/mnt/linuxlab/home/cbarbes1/COSC425-DATA/')
 
 from GeneralUtilities.file_ops.file_ops import FileOps
 from typing import Tuple
@@ -10,7 +10,7 @@ class AbstractCategoryMap:
     def __init__(self, Utilities_obj: object, *, dir_path: str):
         self.utilities = Utilities_obj
         self.dir_path = dir_path
-        self.file_ops = FileOps(output_dir="~/Desktop/COSC425-DATA/PythonCode/Utilities/")
+        self.file_ops = FileOps(output_dir="/mnt/linuxlab/home/cbarbes1/COSC425-DATA/PythonCode/Utilities/")
         self.results = self.map_abstract_categories(dir_path=self.dir_path)
         self.file_ops.write_json("abstracts_to_categories.json", self.results)
 
@@ -28,8 +28,8 @@ class AbstractCategoryMap:
 
             abstract, categories, title = self.extract_abstract_and_categories(attributes=attributes)
             if abstract:
-                results[abstract] = {
-                    'title': title,
+                results[title] = {
+                    'abstract': abstract,
                     'categories': categories
                 }
 
