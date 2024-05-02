104f68ed6e895ce6f7ee61ace25e6c3667b3c7e5
Jude Maggitti <judemaggitti123@gmail.com>
2024-05-02 09:08:16 -0400

    added citation average

--- PythonCode/Utilities/AbstractCategoryMap.py
+++ PythonCode/Utilities/AbstractCategoryMap.py
@@ -1,7 +1,7 @@
 import os
 
 import sys
-sys.path.append('/Users/spencerpresley/COSC425-MAIN/backend')
+sys.path.append('/home/jude/425Project/COSC425-DATA-FORK-1')
 
 from GeneralUtilities.file_ops.file_ops import FileOps
 from typing import Tuple
@@ -10,7 +10,7 @@ class AbstractCategoryMap:
     def __init__(self, Utilities_obj: object, *, dir_path: str):
         self.utilities = Utilities_obj
         self.dir_path = dir_path
-        self.file_ops = FileOps(output_dir="/Users/spencerpresley/COSC425-MAIN/backend/PythonCode/Utilities/")
+        self.file_ops = FileOps(output_dir="/home/jude/425Project/COSC425-DATA-FORK-1/PythonCode/Utilities")
         self.results = self.map_abstract_categories(dir_path=self.dir_path)
         self.file_ops.write_json("abstracts_to_categories.json", self.results)
 
