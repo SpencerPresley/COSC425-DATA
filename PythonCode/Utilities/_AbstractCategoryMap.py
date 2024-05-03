24aee0a22082cc7d9fa8012d29d20b6bcfa3d3ea
SpencerPresley <SpencerPresley96@gmail.com>
2024-05-03 15:27:19 -0400

    priority todo added.

--- PythonCode/Utilities/_AbstractCategoryMap.py
+++ PythonCode/Utilities/_AbstractCategoryMap.py
@@ -1,16 +1,17 @@
 import os
 
 import sys
-sys.path.append('/mnt/linuxlab/home/cbarbes1/COSC425-DATA/')
+sys.path.append('/Users/spencerpresley/COSC425-MAIN/backend/')
 
 from GeneralUtilities.file_ops.file_ops import FileOps
 from typing import Tuple
+
 
 class AbstractCategoryMap:
     def __init__(self, Utilities_obj: object, *, dir_path: str):
         self.utilities = Utilities_obj
         self.dir_path = dir_path
-        self.file_ops = FileOps(output_dir="/mnt/linuxlab/home/cbarbes1/COSC425-DATA/PythonCode/Utilities/")
+        self.file_ops = FileOps(output_dir="/Users/spencerpresley/COSC425-MAIN/backend/PythonCode/Utilities")
         self.results = self.map_abstract_categories(dir_path=self.dir_path)
         self.file_ops.write_json("abstracts_to_categories.json", self.results)
 
