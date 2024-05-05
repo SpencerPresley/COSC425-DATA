b5d52dfbfdcd75d7db17199e7cf4c34e9fddb5b9
SpencerPresley <SpencerPresley96@gmail.com>
2024-05-04 20:01:02 -0400

    code for influential faculty done. Results are in processed_faculty_stats_data.json

--- PythonCode/Utilities/category_processor.py
+++ PythonCode/Utilities/category_processor.py
@@ -1,13 +1,24 @@
 from utilities import Utilities
 import warnings
 from My_Data_Classes import CategoryInfo
+from generate_aux_stats import FacultyStats, FacultyInfo
+
+# Imported for type hinting.
+# Actual object is passed in to CategoryProcessor's constructor
+# via dependency injection.
+from faculty_department_manager import FacultyDepartmentManager
 
 
 class CategoryProcessor:
     def __init__(self, utils, faculty_department_manager):
-        self.utils = utils
-        self.faculty_department_manager = faculty_department_manager
-        self.category_counts = {}
+        self.utils: Utilities = utils
+        self.faculty_department_manager: FacultyDepartmentManager = (
+            faculty_department_manager
+        )
+        self.category_counts: dict[str, CategoryInfo] = {}
+
+        # influential stats dictionaries
+        self.faculty_stats: dict[str, FacultyStats] = {}
 
     def category_finder(self, current_file, file_path):
         file_content = current_file.read()
@@ -17,18 +28,93 @@ class CategoryProcessor:
                 self.update_category_stats(file_path, file_content, lines)
 
     def update_category_stats(self, file_path, file_content, lines):
+        # get attributes from the file
         attribute_results = self.get_attributes(file_content)
         categories = self.get_categories(file_path, attribute_results)
         faculty_members = self.get_faculty_members(attribute_results)
         department_members = self.get_department_members(attribute_results)
+        title = self.get_title(attribute_results)
+
+        # update the category counts dict with the new data
         self.update_faculty_set(categories, faculty_members)
         self.update_department_set(categories, department_members)
         self.update_article_set()
-        title = self.get_title(attribute_results)
         self.update_title_set(categories, title)
+
+        # citation related
+        tc_count: int = self.get_tc_count(lines)
         self.update_tc_list(lines, self.category_counts, categories)
         self.update_tc_count(self.category_counts, categories)
         self.set_citation_average(self.category_counts, categories)
+
+        # construct influential stats
+        # On the entry take the faculty members we got and then += their total citations
+        # += their article count
+        # update article citation map
+        # look up by name variation, and update name to the key string
+        # that way i can match what's in the existing data
+        self.update_faculty_members_stats(
+            faculty_stats=self.faculty_stats,
+            categories=categories,
+            faculty_members=faculty_members,
+            tc_count=tc_count,
+            title=title,
+        )
+
+    @staticmethod
+    def update_faculty_members_stats(
+        *,
+        faculty_stats: dict[str, FacultyStats],
+        categories: list[str],
+        faculty_members: list[str],
+        tc_count: int,
+        title: str,
+    ):
+        # Convert list to set to remove duplicates to avoid double counting
+        faculty_members = set(faculty_members)
+
+        # loop through categories
+        for category in categories:
+
+            # ensure there's a FacultyStats object for the category and that the category exists
+            if category not in faculty_stats:
+                faculty_stats[category] = FacultyStats()
+
+            category_faculty_stats = faculty_stats[category].faculty_stats
+
+            for faculty_member in faculty_members:
+                if faculty_member not in category_faculty_stats:
+                    category_faculty_stats[faculty_member] = FacultyInfo()
+
+                member_info = category_faculty_stats[faculty_member]
+
+                # update members total citations and article count for the category
+                member_info.total_citations += tc_count
+                member_info.article_count += 1
+
+                # update members average citations for the category
+                if member_info.article_count > 0:
+                    member_info.average_citations = (
+                        member_info.total_citations // member_info.article_count
+                    )
+
+                # update the members article citation map for the category
+                member_info.citation_map.article_citation_map[title] = tc_count
+
+                # faculty_info: dict[str, FacultyInfo] = faculty_stats[faculty_member].faculty_stats
+
+                # if faculty_member not in faculty_info:
+                #     faculty_info[faculty_member] = FacultyInfo()
+
+                # member_info = faculty_info[faculty_member]
+
+                # member_info.article_count += 1
+                # member_info.total_citations += tc_count
+
+                # if member_info.article_count > 0:
+                #     member_info.average_citations = member_info.total_citations // member_info.article_count
+
+                # member_info.citation_map.article_citation_map[title] = tc_count
 
     def update_title_set(self, categories, title):
         if title is not None:
@@ -74,7 +160,6 @@ class CategoryProcessor:
         self.update_category_counts_files_set(
             categories=categories, file_name=file_path
         )
-
         return categories
 
     def get_faculty_members(self, attribute_results):
@@ -96,6 +181,9 @@ class CategoryProcessor:
                 # Intialize a new CategoryInfo dataclass instance for the given category
                 self.category_counts[category] = CategoryInfo()
 
+    def get_faculty_stats(self):
+        return self.faculty_stats
+
     @staticmethod
     def update_tc_list(lines, category_counts, categories):
         for line in lines:
@@ -110,6 +198,15 @@ class CategoryProcessor:
             for tc in category_counts[category].tc_list:
                 sum += tc
             category_counts[category].tc_count = sum
+
+    @staticmethod
+    def get_tc_count(lines):
+        tc_count = 0
+        for line in lines:
+            if line.startswith("TC"):
+                tc_count += int(line[3:])
+
+        return tc_count
 
     def update_category_counts_files_set(self, categories, file_name):
         for category in categories:
