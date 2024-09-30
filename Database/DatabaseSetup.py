f1b86c2441d1c1b08d14a2427b34410e5ca88a1c
Cole <cbarbes02@gmail.com>
2024-09-30 15:59:03 -0400

    added database insertion code

---
+++ Database/DatabaseSetup.py
@@ -0,0 +1,46 @@
+import datetime   # This will be needed later
+import os
+import json
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from models import CategoryName, Category, CategoryOut
+from pprint import pprint
+
+load_dotenv()
+mongodb_url = os.getenv("MONGODB_URL")
+
+class DatabaseSetup:
+    def __init__(self, mongo_url: str):
+        client = MongoClient(mongodb_url, server_api=ServerApi('1'))
+        # Send a ping to confirm a successful connection
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+
+    def dump_category_data(self, category_data: list[dict]):
+        client = MongoClient(mongodb_url, server_api=ServerApi('1'))
+        db = client["Site_Data"]
+        collection = db["Categories"]
+        collection.insert_many(category_data)
+        print("Data inserted successfully")
+        client.close()
+
+    def update_category_data(self, )
+
+
+if __name__ == "__main__":
+    with open("processed_category_data.json", "r") as file:
+        data = json.load(file)
+
+    db_setup = DatabaseSetup(mongodb_url)
+    
+    category_data = []
+    for key, value in data.items():
+        value["name"] = key
+        db_output = CategoryOut(**value)
+        category_data.append(db_output.dict())
+
+    db_setup.dump_category_data(category_data)
---
+++ Database/models.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional, Dict, Any
+
+class Category(BaseModel):
+    url: str = Field(..., description="URL of the category")
+    faculty_count: int = Field(..., description="Number of faculty members in the category")
+    department_count: int = Field(..., description="Number of departments in the category")
+    article_count: int = Field(..., description="Number of articles in the category")
+    faculty: List[str] = Field(..., description="List of faculty members in the category")
+    departments: List[str] = Field(..., description="List of departments in the category")
+    titles: List[str] = Field(..., description="List of article titles in the category")
+    tc_count: int = Field(..., description="Total number of citations in the category")
+    citation_average: int = Field(..., description="Average number of citations in the category")
+
+class CategoryName(BaseModel):
+    name: str = Field(..., description="Name of the category")
+    data: Category = Field(..., description="Category data")
+
+class CategoryOut(BaseModel):
+    name: str = Field(..., description="Name of the category")
+    url: str = Field(..., description="URL of the category")
+    faculty_count: int = Field(..., description="Number of faculty members in the category")
+    department_count: int = Field(..., description="Number of departments in the category")
+    article_count: int = Field(..., description="Number of articles in the category")
+    faculty: List[str] = Field(..., description="List of faculty members in the category")
+    departments: List[str] = Field(..., description="List of departments in the category")
+    titles: List[str] = Field(..., description="List of article titles in the category")
+    tc_count: int = Field(..., description="Total number of citations in the category")
+    citation_average: int = Field(..., description="Average number of citations in the category")
