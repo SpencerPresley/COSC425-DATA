4b130fd6827d78de33bc2f7f1eec0fd0f3f4bf8b
Spencer Presley <153392395+SpencerPresley@users.noreply.github.com>
2024-11-22 12:28:54 -0500

    config init update, try_to_insert_cats.py db test script

---
+++ src/academic_metrics/configs/__init__.py
@@ -0,0 +1,14 @@
+from .global_config import (
+    # Main functions
+    configure_logging,
+    set_log_to_console,
+    # Configuration flags
+    LOG_TO_CONSOLE,
+    LOG_LEVEL,
+    # Log levels for use in other modules
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    CRITICAL,
+)
