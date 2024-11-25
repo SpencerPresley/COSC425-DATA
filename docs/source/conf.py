8ea756b52d5cb476a46d24e6853e26d2ebfe656b
Spencer Presley <153392395+SpencerPresley@users.noreply.github.com>
2024-11-24 23:23:53 -0500

    d

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.insert(0, os.path.abspath("../../"))
 
 project = "Academic Metrics"
 copyright = "2024, Spencer Presley, Cole Barbes"
